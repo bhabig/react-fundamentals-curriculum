c985ab1a01ae99825de544f4e54afa78dd23b2d2
Bob Habig <rfhab2@gmail.com>
2018-03-09 19:01:14 -0500

    home page done, except forms not yet live

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import SearchForm from './SearchForm';
 
 const Home = (props) => {
   return (
     <div id="home">
-      <p id="alexa">'Alexa, what's the weather today?'</p>
-      <p>...What's The Weather? is easy, but not quite that easy</p>
-      <h3>Just type a location below and we'll do our best impersonation!</h3>
+      <p id="alexa">"Alexa, what's the weather today?"</p>
+      <p id="home-text">It's easy, but not <em>that</em> easy<br /><br /> Enter your location below:</p>
+      <div id="home-search">
+        <SearchForm />
+      </div>
     </div>
   )
 }
