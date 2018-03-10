8f9776f675481da7d4802cb3d9cd46f0cb5671e6
Bob Habig <rfhab2@gmail.com>
2018-03-10 00:51:01 -0500

    home page is perfect. added tab icon as well. time to work on components

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,11 @@
+'use strict';
+
 import React from 'react';
 import Home from './Home';
-import Header from './Header';
 
 const App = (props) => {
   return (
     <div>
-      <Header />
       <Home />
     </div>
   )
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,16 +1,39 @@
+'use strict';
+
 import React from 'react';
 import SearchForm from './SearchForm';
+import Header from './Header';
 
-const Home = (props) => {
-  return (
-    <div id="home">
-      <p id="alexa">"Alexa, what's the weather today?"</p>
-      <p id="home-text">It's easy, but not <em>that</em> easy<br /><br /> Enter your location below:</p>
-      <div id="home-search">
-        <SearchForm />
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit (location) {
+    this.setState(() => {
+      return {
+        x: ''
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <div id="home">
+          <p id="alexa">"Alexa, what's the weather today?"</p>
+          <p id="home-text1">It's easy, but not <em>that</em> easy</p><br /><p id="home-text2">Enter your location below:</p>
+          <div id="home-search">
+            <SearchForm />
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 module.exports = Home;
